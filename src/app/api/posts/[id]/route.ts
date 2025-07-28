1c4b0be559bb4e74a65b616b4ab7d1517ce0e40d
Yura <yurii.st11@gmail.com>
2025-07-28 17:56:23 +0300

    fix delete api

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,8 +1,8 @@
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function DELETE(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { id: string } }
 ) {
   const id = params.id;
