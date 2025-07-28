7026a0a8df43c55292f0817e8c190a1533df3c30
Yura <yurii.st11@gmail.com>
2025-07-28 18:04:31 +0300

    fix delete api

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = params.id;
+  const { id } = await params;
 
   if (!id) {
     return NextResponse.json({ error: "Invalid post ID" }, { status: 400 });
